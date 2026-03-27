9a715cacbb4f1bee58a364ab961b848e48defbf4
23suryaa <163604785+23suryaa@users.noreply.github.com>
2026-03-26 18:45:57 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/suryaavfx-08ac599f/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
